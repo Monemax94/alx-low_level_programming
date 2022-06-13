8a49469c4e9c3b60fbc7dd250890350ffbc62366
Monemax94 <mondayodoh94@gmail.com>
2022-06-13 09:39:30 -0700

    _puts prints a string to the stdout followed by new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to stdout followed by new line
+ *@str: pointer to the string to be printed
+ */
+void _puts(char *str);
+{
+	int a;
+
+	for (i = 0; str[a] != '\0'; a++)
+	{
+		_putchar(str[a];
+	}
+	_putchar ('\n')
+}
