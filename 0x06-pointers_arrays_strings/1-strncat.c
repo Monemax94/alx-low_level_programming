95a109f66b11bd882cc49755d723d3968218aace
Monemax94 <mondayodoh94@gmail.com>
2022-06-15 10:07:21 -0700

    string concatenation function

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates n bytes from a string to another
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes of string to concatenate
+ *
+ * Return: a pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int d, s;
+
+	d = 0;
+	s = 0;
+
+	while (dest[d] != '\0')
+		d++;
+
+	while (src[s] != '\0' && j < n)
+	{
+		d++;
+		s++;
+	}
+	dest[d] = '\0';
+
+	return (dest);
+}
