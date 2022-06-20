075b31624984fc8b5651c1b2c4c6b379901a1743
Monemax94 <mondayodoh94@gmail.com>
2022-06-20 08:27:22 -0700

    first commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,7 +2,7 @@
 
 /**
  * *_memset - fills memory with a constant byte
- * @s: memory area to be filled 
+ * @s: memory area to be filled
  * @b: char to copy
  * @n: number of times to copy b
  *
@@ -12,11 +12,9 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int j;
 
-	for(j = 0; j < n;j++)
+	for (j = 0; j < n; j++)
 	{
 		s[j] = b;
 	}
-
 	return (s);
-
 }
