339d25f77db813bda796beb5d42781c00874c067
Monemax94 <mondayodoh94@gmail.com>
2022-07-06 08:13:38 -0700

    Task 0 completed

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,9 @@
 #include "function_pointers.h"
 
 /**
- * print_name - is a function that prints a name 
+ * print_name - is a function that prints a name
  * @name: pointer to name input
- * @f: pointer to void function 
+ * @f: pointer to void function
  * Return: void
  */
 void print_name(char *name, void (*f)(char *))
