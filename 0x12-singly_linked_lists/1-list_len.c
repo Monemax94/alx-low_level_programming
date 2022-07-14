47558fe2abedaaf0b19673b1b1373516f5025231
Monemax94 <mondayodoh94@gmail.com>
2022-07-14 09:38:40 -0700

    Task 1 list_len: function that returns the number of elements in a linked list_t list.

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,7 +5,7 @@
  * list_len - prints the number of elements in a linked list
  * @h: pointer to the list_t list
  *
- * Return: number of elements in h 
+ * Return: number of elements in h
  */
 size_t list_len(const list_t *h)
 {
