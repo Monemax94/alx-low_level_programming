06d66e9eb688ae958f3efe5967d4224e5dab20b7
Monemax94 <mondayodoh94gmail.com>
2022-08-09 19:37:57 +0100

    Hello_world updated

---
+++ 0x00-hello_world/7-putting.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzel\n");
+	puts("\"Programming is like building a multilingual puzzel");
+	puts("Programming is like building a multilingual puzzel\n");
+
+	/*my return function below here*/
+	return (0);
+}
